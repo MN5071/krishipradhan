cadc327468dea52bfe402f1a187a1ffaa72e0825
Puneeth MN <puneethpune2004507@gmail.com>
2026-06-01 14:13:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Replace these values before showing the live demo.
 module.exports = {
   GOOGLE_PLACES_API_KEY: '',
-  WHATSAPP_AGENT_NUMBER: '919876543210',
+  WHATSAPP_AGENT_NUMBER: '917022621585',
   DEFAULT_SEARCH_RADIUS_METERS: 7000,
   DEFAULT_STORE_SEARCH_ADDRESS: 'Karnataka, India'
 };
